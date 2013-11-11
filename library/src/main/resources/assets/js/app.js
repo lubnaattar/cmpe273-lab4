de6e78115b4419d5cc5b49dd37cc92d892d647fb
lubnaattar <lubna.attar@gmail.com>
2013-11-10 19:13:10 -0800

    Lab 4

---
+++ library/src/main/resources/assets/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+		
+	
+
+	  $('id^=bookStatus_').
+});
+
+
+$(":button").click(function() {
+    var isbn = this.id;
+   // alert('About to report lost on ISBN ' + isbn);    
+    $.ajax({        
+        url: 'v1/books/'+isbn+'?status=lost',
+        type: 'PUT'      
+        //
+    });
+    
+   
+    this.disabled=true;
+   
+    $('#bookStatus_'+isbn).html("lost");    
+    
+    /*$('#tableId').each(function() {
+    	$('#isbn').find('tr').each(function() {
+    	
+    		var customerId = $(this).find("td").eq(4).html();
+            customerId="lost";
+            $(this).find("td").eq(4).html(customerId);
+            alert("changed"+$(this).find("td").eq(4).html());
+            alert(customerId);
+    	}
+    }        
+    });   
+*/     
+    
+});
